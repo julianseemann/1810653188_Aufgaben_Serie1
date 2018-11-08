3d093d6ac200b3c63e821c122479472c4ce34050
Seemann Julian Max Georg <1810653188@stud.fh-kufstein.ac.at>
2018-11-08 10:49:40 +0100

    Aufgabe 3 ue1

---
+++ src/seue3p/seue3p.java
@@ -0,0 +1,12 @@
+package seue3p;
+
+
+public class seue3p {
+
+    public static void main(String[] args) {
+        String name = "Hans";
+        System.out.println(name);
+        int alter = 20;
+        System.out.println(alter);
+    }
+}
