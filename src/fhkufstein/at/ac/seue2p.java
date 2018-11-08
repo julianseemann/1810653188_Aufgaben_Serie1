9e44b4df7e0c4b910e79e06686cec8b9b23aa250
Seemann Julian Max Georg <1810653188@stud.fh-kufstein.ac.at>
2018-11-08 10:20:30 +0100

    FH Kufstein

---
+++ src/fhkufstein/at/ac/seue2p.java
@@ -0,0 +1,11 @@
+package fhkufstein.at.ac;
+
+public class seue2p {
+
+    public static void main(String[] args) {
+
+        String schule= "FH Kufstein";
+        System.out.println(schule);
+
+    }
+}
