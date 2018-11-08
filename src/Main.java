ddf1381b53ae3763cac5b5c6ce4db39c1cf8a2ba
Seemann Julian Max Georg <1810653188@stud.fh-kufstein.ac.at>
2018-11-08 10:35:11 +0100

    Kommentare

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-
+// Erstellen einer neuen öffentlicher Klasse
 public class Main {
 
+    // Erstellen einer neuen öffentlichen Main Methode
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        String name= "Julian";
-        System.out.println(name);
-        System.out.println();
+        System.out.println("Hello World!"); //Hello World auf der Konsole aufgeben
+        String name= "Julian"; //Zuweisung eines Begriffes mithilfe eines Datentyps
+        System.out.println(name); //Ausgeben des Begriffes name auf der Konsole
+        System.out.println(); //Eingabe und Ausgabe der Main Methode
     }
 }
--- src/fhkufstein/at/ac/seue2p.java
+++ src/fhkufstein/at/ac/seue2p.java
@@ -1,11 +1,13 @@
-package fhkufstein.at.ac;
+package fhkufstein.at.ac; //Neues package/Ordner wird erstellt
 
+// Erstellen einer neuen öffentlicher Klasse
 public class seue2p {
 
+    // Erstellen einer neuen öffentlichen Main Methode
     public static void main(String[] args) {
 
-        String schule= "FH Kufstein";
-        System.out.println(schule);
+        String schule= "FH Kufstein"; //Zuweisung eines Begriffes mithilfe eines Datentyps
+        System.out.println(schule); //Ausgeben des Begriffes schule auf der Konsole
 
     }
 }
